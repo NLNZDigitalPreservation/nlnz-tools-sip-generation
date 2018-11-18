b74747ed44f960484e8e32f76890e06397993f2c
Kurt Lenfesty <Kurt.Lenfesty.NZ@gmail.com>
2018-11-19 12:22:52 +1300

    Move groovy dependency back to 2.4.x.
    Gradle 4.10.x relies on groovy 2.4.x. While groovy 2.5.x has helpful classes
    JsonGenerator, using groovy 2.5.x in downstream builds causes issues with the
    gradle builds themselves. For this reason, the use of groovy 2.5.x features
    are avoided until gradle supports groovy 2.5.x in its own dependencies.
    
    core.gradle:
    - Switch from groovy-all:2.5.+ to localGroovy()
    
    SipJsonGenerator.groovy:
    - Use JsonBuilder instead of JsonGenerator to construct the Json for a Sip.
      Leave the original method commented out.
    
    test-sip-1.json:
    - Make the JSON match the same ordering as found in Sip.groovy (for ease of
      human debugging).

--- core/src/main/groovy/nz/govt/natlib/tools/sip/generation/SipJsonGenerator.groovy
+++ core/src/main/groovy/nz/govt/natlib/tools/sip/generation/SipJsonGenerator.groovy
@@ -1,43 +1,95 @@
 package nz.govt.natlib.tools.sip.generation
 
-import groovy.json.JsonGenerator
-import groovy.json.JsonGenerator.Converter
+import groovy.json.JsonBuilder
+
+//import groovy.json.JsonGenerator
+//import groovy.json.JsonGenerator.Converter
 import groovy.json.JsonOutput
 import groovy.util.logging.Slf4j
 import nz.govt.natlib.tools.sip.Sip
 
-import java.time.LocalDateTime
+//import java.time.LocalDateTime
 
 @Slf4j
 class SipJsonGenerator {
 
     static String toJson(Sip sip, boolean prettyPrint = false) {
-        JsonGenerator jsonGenerator = new JsonGenerator.Options()
-                .excludeFieldsByName([ 'localDate', 'LOCAL_DATE_TIME_FORMATTER' ])  // Exclude fields with given name(s).
-                .addConverter(new Converter() {
-                    // LocalDateTime converter
-                    boolean handles(Class<?> type) {
-                        return LocalDateTime.isAssignableFrom(type)
-                    }
-                    Object convert(Object localDateTime, String key) {
-                        return Sip.LOCAL_DATE_TIME_FORMATTER.format((LocalDateTime) localDateTime)
-                    }
+        // groovy 2.5.x u
+        JsonBuilder jsonBuilder = new JsonBuilder()
+        Map<String, Object> jsonRoot = jsonBuilder {
+            title sip.title
+            year sip.year
+            month sip.month
+            dayOfMonth sip.dayOfMonth
 
-                })
-                .addConverter(new Converter() {
-                    // File converter
-                    boolean handles(Class<?> type) {
-                        return File.isAssignableFrom(type)
-                    }
-                    Object convert(Object file, String key) {
-                        File theFile = (File) file
-                        return theFile.getCanonicalPath()
-                    }
-                })
-                .build()  // Create the converter instance.
+            ieEntityType sip.ieEntityType
+            objectIdentifierType sip.objectIdentifierType
+            objectIdentifierValue sip.objectIdentifierValue
+            policyId sip.policyId
 
-        String jsonString = jsonGenerator.toJson(sip)
+            preservationType sip.preservationType
+            usageType sip.usageType
+            digitalOriginal sip.digitalOriginal
+            revisionNumber sip.revisionNumber
+            almaMmsId sip.almaMmsId
 
-        return prettyPrint ? JsonOutput.prettyPrint(jsonString) : jsonString
+            fileWrappers(sip.fileWrappers) { Sip.FileWrapper fileWrapper ->
+                    mimeType fileWrapper.mimeType
+                    file fileWrapper.file == null ? null : fileWrapper.file.getCanonicalPath()
+                    fileOriginalPath fileWrapper.fileOriginalPath
+                    fileOriginalName fileWrapper.fileOriginalName
+                    label fileWrapper.label
+                    creationDate sip.LOCAL_DATE_TIME_FORMATTER.format(fileWrapper.creationDate)
+                    modificationDate sip.LOCAL_DATE_TIME_FORMATTER.format(fileWrapper.modificationDate)
+                    fileSizeBytes fileWrapper.fileSizeBytes
+                    fixityType fileWrapper.fixityType
+                    fixityValue fileWrapper.fixityValue
+            }
+        }
+
+        String jsonString = jsonBuilder.toString()
+
+        String outputString = prettyPrint ? JsonOutput.prettyPrint(jsonString) : jsonString
+
+        log.debug("sip=${sip} generated json=${outputString}")
+
+        return outputString
      }
+
+    /**
+     * Groovy 2.5.x supports JsonGenerator, which provides a much more elegant way to generate JSON from an object
+     * using Converter classes. But gradle 4.10.x uses groovy 2.4, which does not have these classes. Conflicts with
+     * gradle builds means that it's not easy to dependent builds with groovy 2.5 classes. For this reason, this
+     * method is commented out until Gradle runs with groovy 2.5.x.
+     */
+//    static String toJson(Sip sip, boolean prettyPrint = false) {
+//        // groovy 2.5.x u
+//        JsonGenerator jsonGenerator = new JsonGenerator.Options()
+//                .excludeFieldsByName([ 'localDate', 'LOCAL_DATE_TIME_FORMATTER' ])  // Exclude fields with given name(s).
+//                .addConverter(new Converter() {
+//            // LocalDateTime converter
+//            boolean handles(Class<?> type) {
+//                return LocalDateTime.isAssignableFrom(type)
+//            }
+//            Object convert(Object localDateTime, String key) {
+//                return Sip.LOCAL_DATE_TIME_FORMATTER.format((LocalDateTime) localDateTime)
+//            }
+//
+//        })
+//                .addConverter(new Converter() {
+//            // File converter
+//            boolean handles(Class<?> type) {
+//                return File.isAssignableFrom(type)
+//            }
+//            Object convert(Object file, String key) {
+//                File theFile = (File) file
+//                return theFile.getCanonicalPath()
+//            }
+//        })
+//                .build()  // Create the converter instance.
+//
+//        String jsonString = jsonGenerator.toJson(sip)
+//
+//        return prettyPrint ? JsonOutput.prettyPrint(jsonString) : jsonString
+//    }
 }
