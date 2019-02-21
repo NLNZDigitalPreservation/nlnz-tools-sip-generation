d72b4fed2c8b4c4b9ed41ca94b864453a35ea124
Kurt Lenfesty <Kurt.Lenfesty.NZ@gmail.com>
2019-02-22 12:02:54 +1300

    Separate PDFBox and its tests.
    - As there will be multiple PDF validators, create PDFBox-specific validator
      and move the PDFBox test files to their own pdfbox subfolder.
    
    PdfValidatorPdfBox.groovy:
    - Created from PdfValidator.groovy to reflect PDFBox-specific validation.
    
    - PdfValidatorTest.groovy renamed to PdfValidatorPdfBoxTest.groovy, as it is a
      PDFBox-specific test. Updated the test to reflect the changes.

--- core/src/test/groovy/nz/govt/natlib/tools/sip/pdf/PdfValidatorPdfBoxTest.groovy
+++ core/src/test/groovy/nz/govt/natlib/tools/sip/pdf/PdfValidatorPdfBoxTest.groovy
@@ -3,9 +3,8 @@ package nz.govt.natlib.tools.sip.pdf
 import nz.govt.natlib.tools.sip.state.SipProcessingException
 import nz.govt.natlib.tools.sip.state.SipProcessingExceptionReason
 import nz.govt.natlib.tools.sip.state.SipProcessingExceptionReasonType
+import org.junit.Before
 import org.junit.Test
-
-import java.nio.file.Path
 
 import static org.hamcrest.core.Is.is
 import static org.junit.Assert.assertNotNull
@@ -15,31 +14,45 @@ import static org.junit.Assert.assertThat
 /**
  * Files are borrowed from Apache PDFBox (https://github.com/apache/pdfbox).
  */
-class PdfValidatorTest {
-    static final String TEST_FILE_LOCATION = "src/test/resources/nz/govt/natlib/tools/sip/pdf/"
+class PdfValidatorPdfBoxTest {
+    static final String TEST_FILE_LOCATION = "src/test/resources/nz/govt/natlib/tools/sip/pdf/pdfbox/"
 
     static final String PDF_MIDDLE_CONTROL_CHAR = "PDFAMetaDataValidationTestMiddleControlChar.pdf"
     static final String PDF_MIDDLE_CONTROL_CHAR_REASON =
-            'The given file=src/test/resources/nz/govt/natlib/tools/sip/pdf/PDFAMetaDataValidationTestMiddleControlChar.pdf is an invalid PDF. Validation failure(s)=errorCode=7.2, details=Error on MetaData, Author present in the document catalog dictionary doesn\'t match with XMP information.'
+            'The given file=src/test/resources/nz/govt/natlib/tools/sip/pdf/pdfbox/PDFAMetaDataValidationTestMiddleControlChar.pdf is an invalid PDF. Validation failure(s)=errorCode=7.2, details=Error on MetaData, Author present in the document catalog dictionary doesn\'t match with XMP information.'
 
     static final String PDF_MIDDLE_NULL = "PDFAMetaDataValidationTestMiddleNul.pdf"
     static final String PDF_MIDDLE_NULL_REASON =
-            'The given file=src/test/resources/nz/govt/natlib/tools/sip/pdf/PDFAMetaDataValidationTestMiddleNul.pdf is an invalid PDF. Validation failure(s)=errorCode=7.2, details=Error on MetaData, Author present in the document catalog dictionary doesn\'t match with XMP information.'
+            'The given file=src/test/resources/nz/govt/natlib/tools/sip/pdf/pdfbox/PDFAMetaDataValidationTestMiddleNul.pdf is an invalid PDF. Validation failure(s)=errorCode=7.2, details=Error on MetaData, Author present in the document catalog dictionary doesn\'t match with XMP information.'
 
     static final String PDF_TRAILING_CONTROL_CHAR = "PDFAMetaDataValidationTestTrailingControlChar.pdf"
     static final String PDF_TRAILING_CONTROL_CHAR_REASON =
-            'The given file=src/test/resources/nz/govt/natlib/tools/sip/pdf/PDFAMetaDataValidationTestTrailingControlChar.pdf is an invalid PDF. Validation failure(s)=errorCode=7.2, details=Error on MetaData, Author present in the document catalog dictionary doesn\'t match with XMP information.'
+            'The given file=src/test/resources/nz/govt/natlib/tools/sip/pdf/pdfbox/PDFAMetaDataValidationTestTrailingControlChar.pdf is an invalid PDF. Validation failure(s)=errorCode=7.2, details=Error on MetaData, Author present in the document catalog dictionary doesn\'t match with XMP information.'
 
     static final String PDF_TRAILING_NULL = "PDFAMetaDataValidationTestTrailingNul.pdf"
 
     static final String PDF_TRAILING_SPACES = "PDFAMetaDataValidationTestTrailingSpaces.pdf"
     static final String PDF_TRAILING_SPACES_REASON =
-            'The given file=src/test/resources/nz/govt/natlib/tools/sip/pdf/PDFAMetaDataValidationTestTrailingSpaces.pdf is an invalid PDF. Validation failure(s)=errorCode=7.2, details=Error on MetaData, Author present in the document catalog dictionary doesn\'t match with XMP information.'
+            'The given file=src/test/resources/nz/govt/natlib/tools/sip/pdf/pdfbox/PDFAMetaDataValidationTestTrailingSpaces.pdf is an invalid PDF. Validation failure(s)=errorCode=7.2, details=Error on MetaData, Author present in the document catalog dictionary doesn\'t match with XMP information.'
+
+    PdfValidatorPdfBox underTest
+
+    @Before
+    void setup() {
+        underTest = new PdfValidatorPdfBox()
+    }
+
+    @Test
+    void pdfFactoryCorrectlyInstantiatesPdfValidatorPdfBox() {
+        PdfValidator pdfValidator = PdfValidatorFactory.getValidator(PdfValidatorType.PDF_BOX_VALIDATOR)
+
+        assertThat("PdfValidator instance is PdfValidatorPdfBox", pdfValidator.class, is(PdfValidatorPdfBox.class))
+    }
 
     @Test
     void middleControlCharIsInvalidWithCorrectMessage() {
         File pdfFile = new File(TEST_FILE_LOCATION + PDF_MIDDLE_CONTROL_CHAR)
-        SipProcessingException sipProcessingException = PdfValidator.validatePdf(pdfFile.toPath())
+        SipProcessingException sipProcessingException = underTest.validatePdf(pdfFile.toPath())
 
         assertNotNull("PDF validation produced an exception", sipProcessingException)
 
@@ -54,7 +67,7 @@ class PdfValidatorTest {
     @Test
     void middleNullIsInvalidWithCorrectMessage() {
         File pdfFile = new File(TEST_FILE_LOCATION + PDF_MIDDLE_NULL)
-        SipProcessingException sipProcessingException = PdfValidator.validatePdf(pdfFile.toPath())
+        SipProcessingException sipProcessingException = underTest.validatePdf(pdfFile.toPath())
 
         assertNotNull("PDF validation produced an exception", sipProcessingException)
 
@@ -69,7 +82,7 @@ class PdfValidatorTest {
     @Test
     void trailingControlCharIsInvalidWithCorrectMessage() {
         File pdfFile = new File(TEST_FILE_LOCATION + PDF_TRAILING_CONTROL_CHAR)
-        SipProcessingException sipProcessingException = PdfValidator.validatePdf(pdfFile.toPath())
+        SipProcessingException sipProcessingException = underTest.validatePdf(pdfFile.toPath())
 
         assertNotNull("PDF validation produced an exception", sipProcessingException)
 
@@ -84,7 +97,7 @@ class PdfValidatorTest {
     @Test
     void trailingNullIsValid() {
         File pdfFile = new File(TEST_FILE_LOCATION + PDF_TRAILING_NULL)
-        SipProcessingException sipProcessingException = PdfValidator.validatePdf(pdfFile.toPath())
+        SipProcessingException sipProcessingException = underTest.validatePdf(pdfFile.toPath())
 
         assertNull("PDF validation did not produce an exception", sipProcessingException)
     }
@@ -92,7 +105,7 @@ class PdfValidatorTest {
     @Test
     void trailingSpacesIsInvalidWithCorrectMessage() {
         File pdfFile = new File(TEST_FILE_LOCATION + PDF_TRAILING_SPACES)
-        SipProcessingException sipProcessingException = PdfValidator.validatePdf(pdfFile.toPath())
+        SipProcessingException sipProcessingException = underTest.validatePdf(pdfFile.toPath())
 
         assertNotNull("PDF validation produced an exception", sipProcessingException)
 
