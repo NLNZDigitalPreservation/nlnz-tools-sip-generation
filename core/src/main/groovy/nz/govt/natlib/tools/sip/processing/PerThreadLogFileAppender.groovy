85563b18a795a94ff4cfb0cf041ce2d664e3449f
Kurt Lenfesty <Kurt.Lenfesty.NZ@gmail.com>
2019-06-11 19:54:12 +1200

    Per-thread logging more thread safe and ignores exceptions.
    PerThreadLogFileAppender.groovy:
    - Made more thread safe by putting re-entrant locks around operations.
    - Name of appender can now includes a unique id, which may help in tracking a
      particular appender's use.
    - Created with ignoreExceptions set to true so that any errors logging
      through the appender after it has stopped will be ignored.

--- core/src/main/groovy/nz/govt/natlib/tools/sip/processing/PerThreadLogFileAppender.groovy
+++ core/src/main/groovy/nz/govt/natlib/tools/sip/processing/PerThreadLogFileAppender.groovy
@@ -15,15 +15,16 @@ import org.apache.logging.log4j.core.layout.PatternLayout
 import org.apache.logging.log4j.core.util.KeyValuePair
 
 import java.text.SimpleDateFormat
+import java.util.concurrent.locks.ReentrantLock
 
 /**
  * Logs messages to a specific file for a specific thread.
- *
- * Note that there is an implicit assumption that the thread name does not
- * change while a given appender is active.
  */
 @Log4j2
 class PerThreadLogFileAppender {
+    private static final long DEFAULT_START_WAIT_MILLISECONDS = 50L
+    private static final long DEFAULT_STOP_WAIT_MILLISECONDS = 50L
+    private static final ReentrantLock OPERATIONAL_LOCK = new ReentrantLock()
 
     // c{precision} or logger{precision} - logger{1} is just class name
     // d{pattern} or date{pattern} - date with %date{DEFAULT_MICROS} as 2012-11-02 14:34:02,123456
@@ -38,92 +39,146 @@ class PerThreadLogFileAppender {
     static final String DEFAULT_PATTERN = "%date{DEFAULT_MICROS} [%threadName] %-5level %logger{1}: %message%n%throwable"
     static final String DEFAULT_PATTERN_WITH_LINE_NUMBERS = "%date{DEFAULT_MICROS} [%threadName] %-5level %logger{1}.%method:%line - %message%n%throwable"
 
-    static final String THREAD_CONTEXT_KEY = "threadName"
+    static final String LOG_APPENDER_NAME_KEY = "PerThreadLogFileAppender_Name"
     static final String FILE_SUFFIX = ".log"
     static final SimpleDateFormat LOG_FILE_DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-SSS")
 
-    static File startWithGeneratedFilename(File sourceFolder, String filenamePrefix) {
-        String postFix = "${LOG_FILE_DATE_FORMATTER.format(new Date())}${FILE_SUFFIX}"
-        String filename = "${filenamePrefix}_${postFix}"
+    /**
+     * This must be called on the thread to start logging.
+     * The uniqueID means that the appender name is unique and not re-used.
+     */
+    static File startWithGeneratedFilename(File sourceFolder, String filenamePrefix, UUID uniqueId = null) {
+        OPERATIONAL_LOCK.lock()
+        File logFile
+        try {
+            String postFix = "${LOG_FILE_DATE_FORMATTER.format(new Date())}${FILE_SUFFIX}"
+            String filename = "${filenamePrefix}_${postFix}"
 
-        return start(sourceFolder, filename)
+            logFile = start(sourceFolder, filename, uniqueId)
+        } finally {
+            OPERATIONAL_LOCK.unlock()
+        }
+        return logFile
     }
 
     /**
      * This must be called on the thread to start logging.
+     * The uniqueID means that the appender name is unique and not re-used.
      */
-    static synchronized File start(File sourceFolder, String filename) {
+    static File start(File sourceFolder, String filename, UUID uniqueID = null) {
         File logFile = new File(sourceFolder, filename)
+        OPERATIONAL_LOCK.lock()
 
-        String threadName = Thread.currentThread().getName()
-        ThreadContext.put(THREAD_CONTEXT_KEY, threadName)
+        try {
+            String logAppenderName = logAppenderName(uniqueID)
+            ThreadContext.put(LOG_APPENDER_NAME_KEY, logAppenderName)
 
-        setupAppenderForFile(logFile)
-
+            setupAppenderForFile(logFile, logAppenderName)
+        } finally {
+            OPERATIONAL_LOCK.unlock()
+        }
         return logFile
-    }
-
-    static String currentThreadFileAppenderName() {
-        return "FILE-${Thread.currentThread().getName()}".toString()
     }
 
     /**
      * This must be called on the thread to stop logging.
+     * The uniqueID means that the appender name is unique and not re-used.
+     * If you see errors logged to the effect of:
+     *     'Attempted to append to non-started appender'
+     * it could mean that the ignoreExceptions on the appender has somehow been set to false
+     * (otherwise errors logging to a stopped appender will be ignored).
      */
-    static synchronized void stopAndRemove() {
-        final LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false)
-        final AbstractConfiguration configuration = (AbstractConfiguration) loggerContext.getConfiguration()
+    static void stopAndRemove(UUID uniqueID = null) {
+        OPERATIONAL_LOCK.lock()
+        try {
+            final LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false)
+            final AbstractConfiguration configuration = (AbstractConfiguration) loggerContext.getConfiguration()
 
-        String appenderName = currentThreadFileAppenderName()
-        Appender retrievedAppender = configuration.getAppender(appenderName)
-        if (retrievedAppender == null) {
-            log.warn("Unable to retrieve expected appender=${appenderName}")
-        } else {
-            configuration.removeAppender(appenderName)
-            retrievedAppender.stop()
+            String appenderName = logAppenderName(uniqueID)
+            Appender retrievedAppender = configuration.getAppender(appenderName)
+            if (retrievedAppender == null) {
+                log.warn("Unable to retrieve expected appender=${appenderName}")
+            } else {
+                log.info("Removing logger appender=${appenderName}")
+                configuration.removeAppender(appenderName)
+                loggerContext.getRootLogger().removeAppender(retrievedAppender)
+                loggerContext.updateLoggers()
+                retrievedAppender.stop()
+                while (!retrievedAppender.isStopped()) {
+                    sleep(DEFAULT_STOP_WAIT_MILLISECONDS)
+                }
+                retrievedAppender = configuration.getAppender(appenderName)
+                if (retrievedAppender != null) {
+                    log.warn("Log appender=${appenderName} not null when re-retrieved after removal, retrieved=${retrievedAppender}")
+                }
+                log.info("Removed logger appender=${appenderName}")
+            }
+            ThreadContext.remove(LOG_APPENDER_NAME_KEY)
+        } finally {
+            OPERATIONAL_LOCK.unlock()
         }
-        ThreadContext.remove(THREAD_CONTEXT_KEY)
-
-        loggerContext.updateLoggers()
     }
 
-    static synchronized void setupAppenderForFile(File logFile) {
-        final LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false)
-        final Configuration configuration = loggerContext.getConfiguration()
+    static void setupAppenderForFile(File logFile, String logAppenderName) {
+        OPERATIONAL_LOCK.lock()
+        try {
+            final LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false)
+            final Configuration configuration = loggerContext.getConfiguration()
 
-        Layout layout = PatternLayout.newBuilder().
-                withPattern(DEFAULT_PATTERN).
-                withConfiguration(configuration).
-                build()
+            Appender retrievedAppender = configuration.getAppender(logAppenderName)
+            if (retrievedAppender != null) {
+                log.warn("Attempting to setup appender with name=${logAppenderName}, file=${logFile}, but it already exists=${retrievedAppender}")
+                log.warn("Stopping and removing existing appender and then creating a new one.")
+                stopAndRemove()
+            }
 
-        KeyValuePair threadNamePair = KeyValuePair.newBuilder()
-                .setKey(THREAD_CONTEXT_KEY)
-                .setValue(Thread.currentThread().getName())
-                .build()
-        KeyValuePair[] filterKeyValuePairs = [ threadNamePair ]
-        Filter threadContextMapFilter = ThreadContextMapFilter.createFilter(filterKeyValuePairs, "or",
-                Filter.Result.ACCEPT, Filter.Result.DENY)
+            Layout layout = PatternLayout.newBuilder().
+                    withPattern(DEFAULT_PATTERN).
+                    withConfiguration(configuration).
+                    build()
 
-        Appender appender = FileAppender.newBuilder()
-                .setName(currentThreadFileAppenderName())
-                .withFileName(logFile.getCanonicalPath())
-                .withAppend(true)
-                .withLocking(false)
-                .withImmediateFlush(true)
-                .withBufferedIo(false)
-                //.withBufferSize(4000)
-                .setLayout(layout)
-                .setFilter(threadContextMapFilter)
-                .setConfiguration(configuration)
-                .setIgnoreExceptions(false)
-                .withAdvertiseUri(null)
-                .build()
+            KeyValuePair threadNamePair = KeyValuePair.newBuilder()
+                    .setKey(LOG_APPENDER_NAME_KEY)
+                    .setValue(logAppenderName)
+                    .build()
+            KeyValuePair[] filterKeyValuePairs = [threadNamePair]
+            Filter threadContextMapFilter = ThreadContextMapFilter.createFilter(filterKeyValuePairs, "or",
+                    Filter.Result.ACCEPT, Filter.Result.DENY)
 
-        configuration.addAppender(appender)
-        loggerContext.getRootLogger().addAppender(appender)
-        loggerContext.updateLoggers()
-        appender.start()
+            Appender appender = FileAppender.newBuilder()
+                    .setName(logAppenderName)
+                    .withFileName(logFile.getCanonicalPath())
+                    .withAppend(true)
+                    .withLocking(false)
+                    .withImmediateFlush(true)
+                    .withBufferedIo(false)
+                    //.withBufferSize(4000)
+                    .setLayout(layout)
+                    .setFilter(threadContextMapFilter)
+                    .setConfiguration(configuration)
+                    // After the appender is stopped we don't want it throwing exceptions if there are attempts to log
+                    // through it.
+                    .setIgnoreExceptions(true)
+                    .withAdvertiseUri(null)
+                    .build()
+
+            log.info("Adding logger appender=${logAppenderName}")
+            appender.start()
+            while (!appender.isStarted()) {
+                sleep(DEFAULT_START_WAIT_MILLISECONDS)
+            }
+            configuration.addAppender(appender)
+            loggerContext.getRootLogger().addAppender(appender)
+            loggerContext.updateLoggers()
+            log.info("Added logger appender=${logAppenderName}")
+        } finally {
+            OPERATIONAL_LOCK.unlock()
+        }
     }
 
+    static String logAppenderName(UUID uuid) {
+        String threadName = Thread.currentThread().getName()
 
+        return uuid == null ? "FILE-${threadName}" : "FILE-${threadName}-${uuid}"
+    }
 }
