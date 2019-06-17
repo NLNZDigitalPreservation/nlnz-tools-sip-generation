20914ce7416aff99892dd32415017e8fd972918d
Kurt Lenfesty <Kurt.Lenfesty.NZ@gmail.com>
2019-06-17 22:23:49 +1200

    PdfDimensionFinder for finding dimensions and ratios.
    PdfDimensionFinder.groovy:
    - Class with methods to find the dimensions of a PDF document. Methods also
      include ratios of one PDF to another and determining if one is half or
      double the width of the other.
    - Added unit tests and test documents to verify functionality.
    
    PdfInformationExtractor.groovy:
    - Wrapped PDDocument loading with a try/final block to ensure the document
      gets closed.

---
+++ core/src/main/groovy/nz/govt/natlib/tools/sip/pdf/PdfDimensionFinder.groovy
@@ -0,0 +1,111 @@
+package nz.govt.natlib.tools.sip.pdf
+
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
+
+import java.awt.Point
+import java.awt.geom.Point2D
+
+class PdfDimensionFinder {
+
+    static Point getDimensions(final File pdfFile, int pageNumber = 0) {
+        Point dimensions = new Point(-1, -1)
+        PDDocument pdDocument
+        try {
+            pdDocument = PDDocument.load(pdfFile)
+            int numberOfPages = pdDocument.getNumberOfPages()
+            if (numberOfPages > pageNumber) {
+                PDPage page = pdDocument.getPage(pageNumber)
+                float height = page.getMediaBox().getHeight()
+                float width = page.getMediaBox().getWidth()
+                // We accept rounding, as we're not trying to be excessively precise.
+                dimensions = new Point((int) width, (int) height)
+            }
+        } finally {
+            if (pdDocument != null) {
+                pdDocument.close()
+            }
+        }
+        return dimensions
+    }
+
+    static Point2D.Double getDimensionalRatio(final File pdfFile1, final File pdfFile2, int pdfFile1Page = 0,
+                                              int pdfFile2Page = 0) {
+        Point pdfFile1Point = getDimensions(pdfFile1, pdfFile1Page)
+        Point pdfFile2Point = getDimensions(pdfFile2, pdfFile2Page)
+
+        return getDimensionalRatio(pdfFile1Point, pdfFile2Point)
+    }
+
+    static Point2D.Double getDimensionalRatio(Point firstSize, Point secondSize) {
+        double xRatio = -1.0
+        double yRatio = -1.0
+        if (firstSize.x > 0 && secondSize.x > 0) {
+            xRatio = ((double) firstSize.x) / ((double) secondSize.x)
+        }
+
+        if (firstSize.y > 0 && secondSize.y > 0) {
+            yRatio = ((double) firstSize.y) / ((double) secondSize.y)
+        }
+
+        return new Point2D.Double(xRatio, yRatio)
+    }
+
+    static boolean isSameSize(Point2D.Double ratio, double minimumVariation) {
+        return isSameHeight(ratio, minimumVariation) && isSameWidth(ratio, minimumVariation)
+    }
+
+    static boolean isSameHeight(Point2D.Double ratio, double minimumVariation) {
+        return minimumVariation > Math.abs(1.0 - ratio.y)
+    }
+
+    static boolean isSameWidth(Point2D.Double ratio, double minimumVariation) {
+        return minimumVariation > Math.abs(1.0 - ratio.x)
+    }
+
+    static boolean isDoubleWidth(Point2D.Double ratio, double minimumVariation) {
+        return minimumVariation > Math.abs(2.0 - ratio.x)
+    }
+
+    static boolean isHalfWidth(Point2D.Double ratio, double minimumVariation) {
+        return minimumVariation > Math.abs(0.5 - ratio.x)
+    }
+
+    static boolean isSameSize(final File pdfFile1, final File pdfFile2, int pdfFile1Page = 0, int pdfFile2Page = 0,
+                              double minimumVariation) {
+        Point2D.Double ratio = getDimensionalRatio(pdfFile1, pdfFile2, pdfFile1Page, pdfFile2Page)
+        return isSameSize(ratio, minimumVariation)
+    }
+
+    static boolean isSameHeight(final File pdfFile1, final File pdfFile2, int pdfFile1Page = 0, int pdfFile2Page = 0,
+                                double minimumVariation = 0.01) {
+        Point2D.Double ratio = getDimensionalRatio(pdfFile1, pdfFile2, pdfFile1Page, pdfFile2Page)
+        return isSameHeightDoubleWidth(ratio, minimumVariation)
+    }
+
+    static boolean isSameWidth(final File pdfFile1, final File pdfFile2, int pdfFile1Page = 0, int pdfFile2Page = 0,
+                               double minimumVariation = 0.01) {
+        Point2D.Double ratio = getDimensionalRatio(pdfFile1, pdfFile2, pdfFile1Page, pdfFile2Page)
+        return isSameWidth(ratio, minimumVariation)
+    }
+
+    static boolean isSameHeightDoubleWidth(final File pdfFile1, final File pdfFile2, int pdfFile1Page = 0,
+                                           int pdfFile2Page = 0, double minimumVariation = 0.01) {
+        Point2D.Double ratio = getDimensionalRatio(pdfFile1, pdfFile2, pdfFile1Page, pdfFile2Page)
+        return isSameHeightDoubleWidth(ratio, minimumVariation)
+    }
+
+    static boolean isSameHeightDoubleWidth(Point2D.Double ratio, double minimumVariation) {
+        return isSameHeight(ratio, minimumVariation) && isDoubleWidth(ratio, minimumVariation)
+    }
+
+    static boolean isSameHeightHalfWidth(final File pdfFile1, final File pdfFile2, int pdfFile1Page = 0,
+                                         int pdfFile2Page = 0, double minimumVariation = 0.01) {
+        Point2D.Double ratio = getDimensionalRatio(pdfFile1, pdfFile2, pdfFile1Page, pdfFile2Page)
+        return isSameHeightHalfWidth(ratio, minimumVariation)
+    }
+
+    static boolean isSameHeightHalfWidth(Point2D.Double ratio, double minimumVariation) {
+        return isSameHeight(ratio, minimumVariation) && isHalfWidth(ratio, minimumVariation)
+    }
+}
--- core/src/main/groovy/nz/govt/natlib/tools/sip/pdf/PdfInformationExtractor.groovy
+++ core/src/main/groovy/nz/govt/natlib/tools/sip/pdf/PdfInformationExtractor.groovy
@@ -13,52 +13,71 @@ class PdfInformationExtractor {
 
     static Map<String, String> extractMetadata(File pdfFile) {
         Map<String, String> metadataMap = [ : ]
-        PDDocument pdDocument = PDDocument.load(pdfFile)
-        PDDocumentInformation info = pdDocument.getDocumentInformation()
+        PDDocument pdDocument
+        try {
+            pdDocument = PDDocument.load(pdfFile)
+            PDDocumentInformation info = pdDocument.getDocumentInformation()
 
-        metadataMap.put("Page Count", "${pdDocument.getNumberOfPages()}")
+            metadataMap.put("Page Count", "${pdDocument.getNumberOfPages()}")
 
-        metadataMap.put("Title", "${info.getTitle()}")
-        metadataMap.put("Author", "${info.getAuthor()}")
-        metadataMap.put("Subject", "${info.getSubject()}")
-        metadataMap.put("Keywords", "${info.getKeywords()}")
-        metadataMap.put("Creator", "${info.getCreator()}")
-        metadataMap.put("Producer", "${info.getProducer()}")
-        metadataMap.put("Creation Date", "${info.getCreationDate()}")
-        metadataMap.put("Modification Date", "${info.getModificationDate()}")
-        metadataMap.put("Trapped", "${info.getTrapped()}")
+            metadataMap.put("Title", "${info.getTitle()}")
+            metadataMap.put("Author", "${info.getAuthor()}")
+            metadataMap.put("Subject", "${info.getSubject()}")
+            metadataMap.put("Keywords", "${info.getKeywords()}")
+            metadataMap.put("Creator", "${info.getCreator()}")
+            metadataMap.put("Producer", "${info.getProducer()}")
+            metadataMap.put("Creation Date", "${info.getCreationDate()}")
+            metadataMap.put("Modification Date", "${info.getModificationDate()}")
+            metadataMap.put("Trapped", "${info.getTrapped()}")
 
-        pdDocument.close()
+        } finally {
+            if (pdDocument != null) {
+                pdDocument.close()
+            }
+        }
         return metadataMap
     }
 
     static String extractText(File pdfFile) {
-        PDDocument pdDocument = PDDocument.load(pdfFile)
-        PDFTextStripper stripper = new PDFTextStripper()
+        String text = ""
+        PDDocument pdDocument
+        try {
+            pdDocument = PDDocument.load(pdfFile)
+            PDFTextStripper stripper = new PDFTextStripper()
 
-        String text = stripper.getText(pdDocument)
-        pdDocument.close()
+            text = stripper.getText(pdDocument)
+        } finally {
+            if (pdDocument != null) {
+                pdDocument.close()
+            }
+        }
 
         return text
     }
 
     static List<String> matchText(File pdfFile, String regexPattern) {
         log.info("matchText regexPattern=${regexPattern} for pdfFile=${pdfFile.getCanonicalPath()}")
-        PDDocument pdDocument = PDDocument.load(pdfFile)
-        PDFTextStripper stripper = new PDFTextStripper()
-
-        String text = stripper.getText(pdDocument)
-
         List<String> matchingLines = [ ]
-        Pattern pattern = Pattern.compile(regexPattern)
-        text.eachLine { String line ->
-            Matcher matcher = pattern.matcher(line)
-            if (matcher.find()) {
-                log.info("Found match for regexPattern=${regexPattern} in line=${line}")
-                matchingLines.add(line)
+        PDDocument pdDocument
+        try {
+            pdDocument = PDDocument.load(pdfFile)
+            PDFTextStripper stripper = new PDFTextStripper()
+
+            String text = stripper.getText(pdDocument)
+
+            Pattern pattern = Pattern.compile(regexPattern)
+            text.eachLine { String line ->
+                Matcher matcher = pattern.matcher(line)
+                if (matcher.find()) {
+                    log.info("Found match for regexPattern=${regexPattern} in line=${line}")
+                    matchingLines.add(line)
+                }
+            }
+        } finally {
+            if (pdDocument != null) {
+                pdDocument.close()
             }
         }
-        pdDocument.close()
 
         return matchingLines
     }
